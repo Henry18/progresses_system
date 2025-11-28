7394f91014d6ca826758940e052851479fcc6f2b
Henry Martinez <martinezhenry18@gmail.com>
2025-11-27 23:31:14 -0500

    [testing] return print

--- core/routes/admin.php
+++ core/routes/admin.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::namespace('Auth')->group(function () {
-    print_r('admin');
-    die();
     Route::middleware('admin.guest')->group(function(){
         Route::controller('LoginController')->group(function () {
             Route::get('/', 'showLoginForm')->name('login');
